08d08cb0e3e42a7c0bcfe3ea55519d555b41b9a8
Rıza Yörük <rizayoruk180@gmail.com>
2022-07-04 07:20:15 +0300

    ..

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', function () {
     return view('home.index',['name'=>'rizayoruk']);
 });
-///////
+///////yazdıklarımız
 Route::get('/', function () {
     return view('home.index');
 });
@@ -25,7 +25,16 @@ Route::get('/home/{id}',[HomeController::class,'test'])->where('id','[0-9]+')->n
 Route::redirect('/anasayfa','/home')->name('anasayfa');
 
 
-///////////
+/// Admin
+
+
+//Route::redirect('/admin','/home')->name('anasayfa');
+Route::get('/admin',[\App\Http\Controllers\Admin\HomeController::class,'index'])->name('adminhome');
+
+
+
+
+/////
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
